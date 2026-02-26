1c17f54ab4aee89663287af5c07db9dce7c1b0db
liton2k1 <litonmia2k1@gmail.com>
2026-02-27 05:46:46 +0600

    variables

---
+++ Variables/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace System;
+
+class Variable
+{
+    public static void Main(string[] args)
+    {
+       string name= "Liton Mia";
+       int age = 30;
+       double height = 5.6;
+       bool isMarried = true;
+       
+       Console.WriteLine(name);
+       Console.WriteLine(age);
+       Console.WriteLine(height);
+       Console.WriteLine(isMarried);
+    }
+}
